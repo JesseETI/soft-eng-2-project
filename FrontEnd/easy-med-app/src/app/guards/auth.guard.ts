ae94c9304d92d4ec27f59d006a0567015663d0a0
ramith123 <ramithchandula@gmail.com>
2020-10-19 20:10:01 -0400

    Signed-off-by: ramith123 <ramithchandula@gmail.com>

--- FrontEnd/easy-med-app/src/app/guards/auth.guard.ts
+++ FrontEnd/easy-med-app/src/app/guards/auth.guard.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { map, take } from 'rxjs/operators';
-import { AuthService } from '../service/auth.service';
+import { AuthService } from './../service/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,7 @@ import { AuthService } from '../service/auth.service';
 export class AuthGuard implements CanActivate{
   constructor(private router: Router, private auth: AuthService, private alertCtrl: AlertController) { }
  
-  canActivate(): Observable<boolean> {
+  canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
     return this.auth.user.pipe(
       take(1),
       map(user => {
