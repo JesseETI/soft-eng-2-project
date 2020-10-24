ea374d54536c46dc4ea4717bb898c44ed1777077
ramith123 <ramithchandula@gmail.com>
2020-10-24 17:45:02 -0400

    added registration back and template

--- FrontEnd/easy-med-app/src/app/register/register.page.ts
+++ FrontEnd/easy-med-app/src/app/register/register.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  constructor(private auth:AuthService,private router: Router, private alertCtrl:AlertController,private toastCtrl:ToastController) { }
 
   ngOnInit() {
+
+  }
+  register(formData:any) {
+    console.log(formData.value);
+    this.auth.register(formData.value).subscribe(async res =>{
+      console.log("Returned register value: " + res);
+      if (res){
+        //if successful register
+        const toast = await this.toastCtrl.create({
+          message: 'Registration complete',
+          duration: 2000
+        });
+        toast.present();
+        this.router.navigateByUrl('/');
+      }else{
+        const alert = await this.alertCtrl.create({
+          header: 'Registration Failed',
+          message: 'try again.',
+          buttons: ['OK']
+        });
+        await alert.present();
+        this.router.navigateByUrl('register');
+      }
+    });
   }
 
 }
--- FrontEnd/easy-med-app/src/app/service/auth.service.ts
+++ FrontEnd/easy-med-app/src/app/service/auth.service.ts
@@ -1,14 +1,16 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { Platform } from '@ionic/angular';
 import {Storage} from '@ionic/storage';
-import { BehaviorSubject, from, Observable, of, throwError } from 'rxjs';
-import {take,map,switchMap, catchError} from 'rxjs/operators';
+import { BehaviorSubject, from, Observable, of} from 'rxjs';
+import {take,map,switchMap, catchError, tap} from 'rxjs/operators';
 const helper = new JwtHelperService();
 const TOKEN_KEY = 'jwt-token';
-const SERVER_URL = 'https://reqres.in/api/login'
+//CHANGEME: Change url's below
+const LOGIN_URL = 'https://reqres.in/api/login'
+const REGISTER_URL = 'https://reqres.in/api/register'
 // DELETEME: A test JWT
 const TEST_JWT_KEY = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImp0aSI6IjU1M2IyZmJmLTFmYzItNDZhZS1iMjc1LWZmNjNiODVjYTFjMyIsImlhdCI6MTYwMzEzNjQxNCwiZXhwIjoxNjAzMTQwMDE0fQ.ZVxhFc0CB-NVspQmb__vUAqWSQXuaxtiY7DHM_isEw4";
 
@@ -48,7 +50,7 @@ export class AuthService {
     // TODO: Add send to server part here
     //DELETEME: the next line
     if (credentials.email !== "root" || credentials.password !== "toor") return of(null);
-    return this.http.post(SERVER_URL,credentials).pipe(
+    return this.http.post(LOGIN_URL,credentials).pipe(
       take(1),
       catchError((err) =>{
         console.log("Server Error..");
@@ -75,6 +77,23 @@ export class AuthService {
 
     );
   
+  }
+
+  register(formData:{email:string, password:string}): Observable<any>{
+    return this.http.post(REGISTER_URL, formData).pipe(
+      take(1),
+      
+      catchError((err) =>{
+        console.log(err);
+        return of(false);
+      }),
+      switchMap(res=>{
+        if (res) return of(true);
+        return of(false);
+      })
+
+
+    )
   }
 
   getUser(){
