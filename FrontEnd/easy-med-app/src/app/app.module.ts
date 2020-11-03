34d61606629ffa506d2b51e230917cc115aa9b42
ramith123 <ramithchandula@gmail.com>
2020-11-03 19:49:47 -0400

    added form basics for order

--- FrontEnd/easy-med-app/src/app/app.module.ts
+++ FrontEnd/easy-med-app/src/app/app.module.ts
@@ -10,15 +10,22 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-
-
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+  imports: [
+    BrowserModule, 
+    ReactiveFormsModule,
+    FormsModule,
+    IonicModule.forRoot(), 
+    AppRoutingModule,
     IonicStorageModule.forRoot(),
-    HttpClientModule],
+    HttpClientModule,
+   
+  ],
+    
   providers: [
     StatusBar,
     SplashScreen,
--- FrontEnd/easy-med-app/src/app/tab1/order-request/order-request.page.ts
+++ FrontEnd/easy-med-app/src/app/tab1/order-request/order-request.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup,FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-order-request',
@@ -6,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-request.page.scss'],
 })
 export class OrderRequestPage implements OnInit {
-
-  constructor() { }
+  myForm: FormGroup;
+  medCount: number;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.medCount=0;
+    this.myForm = this.fb.group({
+      name:new FormControl('', Validators.required),
+      medicines: this.fb.array([]),
+
+
+    })
+  }
+
+  get medicines() {
+    return this.myForm.get('medicines') as FormArray;
+  }
+
+  addMedicine(){
+    const medicine = this.fb.group({
+      //Can be expanded later
+      medInfo:"",
+    })
+    this.medicines.push(medicine);
+  }
+  deleteMedicine(index){
+    this.medicines.removeAt(index); 
+    this.setMedCount(); 
+  }
+  submitOrder(){
+    console.log(this.myForm.value);
+  }
+  setMedCount(){
+    
+    this.medCount=0;
+    const data = this.myForm.value;
+    for (let med of data.medicines){
+      if (med.medInfo.length >0) this.medCount++;
+    }
+    console.log(this.medCount);
   }
 
 }
