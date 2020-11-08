f4bf26898da508be6ed722e2a9b6d07b04ed6571
ramith123 <ramithchandula@gmail.com>
2020-11-08 16:29:03 -0400

    fixed the login not working after refresh issue

--- FrontEnd/easy-med-app/src/app/guards/auth.guard.ts
+++ FrontEnd/easy-med-app/src/app/guards/auth.guard.ts
@@ -1,35 +1,41 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
-import { Observable } from 'rxjs';
-import { map, take } from 'rxjs/operators';
-import { AuthService } from './../service/auth.service';
+import { Injectable } from "@angular/core";
+import { CanActivate, ActivatedRouteSnapshot, Router } from "@angular/router";
+import { AlertController } from "@ionic/angular";
+import { Observable } from "rxjs";
+import { map, take } from "rxjs/operators";
+import { AuthService } from "./../service/auth.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
-export class AuthGuard implements CanActivate{
-  constructor(private router: Router, private auth: AuthService, private alertCtrl: AlertController) { }
- 
+export class AuthGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private auth: AuthService,
+    private alertCtrl: AlertController
+  ) {}
+
   canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
-    return this.auth.userData.pipe(
+    return this.auth.user.pipe(
       take(1),
-      map(user => {
+      map((user) => {
         console.log(user);
         if (!user || user.role !== route.data.role) {
-          this.alertCtrl.create({
-            header: 'Unauthorized',
-            message: 'You are not allowed to access that page.',
-            buttons: ['OK']
-          }).then(alert => alert.present());
- 
-          this.router.navigateByUrl('/');
+          this.alertCtrl
+            .create({
+              header: "Unauthorized",
+              message: "You are not allowed to access that page.",
+              buttons: ["OK"],
+            })
+            .then((alert) => alert.present());
+
+          this.router.navigateByUrl("/");
           return false;
         } else {
           //TODO: add role pages redirection here
           return true;
         }
       })
-    )
+    );
   }
 }
--- FrontEnd/easy-med-app/src/app/service/auth.service.ts
+++ FrontEnd/easy-med-app/src/app/service/auth.service.ts
@@ -1,107 +1,96 @@
-import { HttpClient} from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { JwtHelperService } from '@auth0/angular-jwt';
-import { Platform } from '@ionic/angular';
-import {Storage} from '@ionic/storage';
-import { BehaviorSubject, from, Observable, of} from 'rxjs';
-import {take,map,switchMap, catchError} from 'rxjs/operators';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { JwtHelperService } from "@auth0/angular-jwt";
+import { Platform } from "@ionic/angular";
+import { Storage } from "@ionic/storage";
+import { BehaviorSubject, from, Observable, of } from "rxjs";
+import { take, map, switchMap, catchError, filter } from "rxjs/operators";
 const helper = new JwtHelperService();
-const TOKEN_KEY = 'jwt-token';
+const TOKEN_KEY = "jwt-token";
 //CHANGEME: Change url's below
-const LOGIN_URL = 'https://reqres.in/api/login'
-const REGISTER_URL = 'https://reqres.in/api/register'
+const LOGIN_URL = "https://reqres.in/api/login";
+const REGISTER_URL = "https://reqres.in/api/register";
 // DELETEME: A test JWT
-const TEST_JWT_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UiLCJyb2xlIjoiVVNFUiJ9.606obpuSoyZN8sAhMxZ8H2Uak_9eew8psOeAHHlJH5Y";
+const TEST_JWT_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UiLCJyb2xlIjoiVVNFUiJ9.606obpuSoyZN8sAhMxZ8H2Uak_9eew8psOeAHHlJH5Y";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class AuthService {
   public user: Observable<any>;
-  public userData= new BehaviorSubject(null);
- 
-  constructor(private storage: Storage, private http: HttpClient, 
-    private plt: Platform, private router: Router) { 
-      this.loadStoredToken();
+  public userData = new BehaviorSubject(null);
+
+  constructor(
+    private storage: Storage,
+    private http: HttpClient,
+    private plt: Platform,
+    private router: Router
+  ) {
+    this.loadStoredToken();
+    this.user = this.userData.asObservable().pipe(filter((user) => user)); //filter out null
   }
 
-  loadStoredToken(){
-    let platformObs = from(this.plt.ready());
-    this.user = platformObs.pipe(
-      switchMap(()=> {
-        return from(this.storage.get(TOKEN_KEY));
-      }),
-      map((token:any) =>{
-        console.log("token storage: ", token);
-        if (token) {
-          let decoded = helper.decodeToken(token);
-          console.log('decoded: ',decoded);
-          this.userData.next(decoded);
-          return true;
-        }else{
-          return null;
-        }
-      })
-    );
+  loadStoredToken() {
+    let platformObs = from(this.storage.get(TOKEN_KEY));
+    platformObs.pipe(take(1)).subscribe((token) => {
+      console.log("token storage: ", token);
+      if (token) {
+        let decoded = helper.decodeToken(token);
+        console.log("decoded: ", decoded);
+        this.userData.next(decoded);
+      } else {
+        this.userData.next({ msg: "Not a user" });
+      }
+    });
   }
 
-  login(credentials:{email:string,password:string}): Observable<any>{
-    
-    
-    return this.http.post(LOGIN_URL,credentials).pipe(
+  login(credentials: { email: string; password: string }): Observable<any> {
+    return this.http.post(LOGIN_URL, credentials).pipe(
       take(1),
-      catchError((err) =>{
+      catchError((err) => {
         console.log("Server Error..");
         return of(null);
       }),
-      
-      map((res:any) => {
+
+      map((res: any) => {
         if (res) return TEST_JWT_KEY; //TODO: change to get just the token from server response
         return res; //DELETEME: replace this with return res;
       }),
-      switchMap(token =>{
-        if (token === null) return of(null);
+      switchMap((token) => {
+        if (token == null) return of(null);
         let decoded = helper.decodeToken(token);
-        console.log('login decoded: ' + decoded.values);
-        this.userData.next(decoded);
+        this.user = of(decoded);
 
-        let storageObs = from(this.storage.set(TOKEN_KEY,token));
+        let storageObs = from(this.storage.set(TOKEN_KEY, token));
         return storageObs;
-      }),
-      
-
+      })
     );
-  
   }
 
-  register(formData:{email:string, password:string}): Observable<any>{
+  register(formData: { email: string; password: string }): Observable<any> {
     return this.http.post(REGISTER_URL, formData).pipe(
       take(1),
-      
-      catchError((err) =>{
+
+      catchError((err) => {
         console.log(err);
         return of(false);
       }),
-      switchMap(res=>{
+      switchMap((res) => {
         if (res) return of(true);
         return of(false);
       })
-
-
-    )
+    );
   }
 
-  getUser(){
+  getUser() {
     return this.userData.getValue();
   }
-  logout(){
-    this.storage.remove(TOKEN_KEY).then(()=>{
-      this.router.navigateByUrl('/');
-      this.userData.next(null);
-
+  logout() {
+    this.storage.remove(TOKEN_KEY).then(() => {
+      this.router.navigateByUrl("/");
+      this.userData.next({ msg: "Not a user" });
     });
-
   }
-  
 }
