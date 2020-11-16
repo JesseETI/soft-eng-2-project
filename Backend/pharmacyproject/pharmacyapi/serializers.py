c718bb26e69de2154839894365e5669d890eb90d
JesseETI <jesseemamalie2@gmail.com>
2020-11-16 18:10:32 -0400

    Order & Pharmacy Functionality for backend added

--- Backend/pharmacyproject/pharmacyapi/serializers.py
+++ Backend/pharmacyproject/pharmacyapi/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User, PrescriptonOrder
+from .models import User, PrescriptonOrder, Pharmacy
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -18,4 +18,10 @@ class PrescriptionSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = PrescriptonOrder
+        fields = '__all__'
+
+class PharmacySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Pharmacy
         fields = '__all__'
--- Backend/pharmacyproject/pharmacyapi/views.py
+++ Backend/pharmacyproject/pharmacyapi/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets
-from .models import User, PrescriptonOrder
-from .serializers import UserSerializer, PrescriptionSerializer
-from rest_framework import permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import User, PrescriptonOrder, Pharmacy
+from .serializers import UserSerializer, PrescriptionSerializer, PharmacySerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -10,29 +11,33 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.AllowAny,)
 
 
+class PharmacyViewSet(viewsets.ModelViewSet):
+    queryset = Pharmacy.objects.all()
+    serializer_class = PharmacySerializer
+    permission_classes = (permissions.AllowAny,)
+
+
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = PrescriptonOrder.objects.all()
     serializer_class = PrescriptionSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
-    @action(methods=['GET'], detail = False)
+    @action(methods=['GET'], detail=False)
     def getmyorders(self, request):
         user = request.user
 
         try:
-            if (user.role =="USER"):
-                orders_queryset = PerscriptionOrder.objects.filter(user = user.id)
-            elif (user.role =="PHARM"):
-                pharmacy = Pharmacy.objects.filter(pharmacist = user.id).first()
-                orders_queryset = PerscriptionOrder.objects.filter(pharmacy = pharmacy.id)
+            if user.role == "USER":
+                orders_queryset = PrescriptonOrder.objects.filter(user = user.id)
+            elif user.role == "PHARM":
+                pharmacy = Pharmacy.objects.get(pharmacist = user.id)
+                orders_queryset = PrescriptonOrder.objects.filter(pharmacy=pharmacy.id)
 
-            serializer = OrderSerializer(orders_queryset, many = True)
+            serializer = PrescriptionSerializer(orders_queryset, many=True)
 
             return Response(serializer.data, status=status.HTTP_200_OK)
         except:
             response = {
                 "message": "No order found",
             }
-            return Response(response, status = status.HTTP_400_BAD_REQUEST)
-
-
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
