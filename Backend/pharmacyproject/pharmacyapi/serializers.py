26158a90ce49166a67f2832cf0818a85bdcd7b46
JesseETI <jesseemamalie2@gmail.com>
2020-11-22 15:54:38 -0400

    Fixed Pharmacy for creation of nested pharmacist

--- Backend/pharmacyproject/pharmacyapi/serializers.py
+++ Backend/pharmacyproject/pharmacyapi/serializers.py
@@ -26,3 +26,9 @@ class PharmacySerializer(serializers.ModelSerializer):
     class Meta:
         model = Pharmacy
         fields = ('name', 'address', 'contact', 'pharmacist')
+
+    def create(self, validated_data):
+        pharmacist_data = validated_data.pop('pharmacist')
+        pharmacist = User.objects.create(**pharmacist_data)
+        pharmacy = Pharmacy.objects.create(pharmacist= pharmacist, **validated_data)
+        return pharmacy
